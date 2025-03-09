fa8331a6596f559b79d9828d2085d9f46a7ec917
William M. Tsikata <tsikatawill@gmail.com>
2025-03-09 16:23:08 +0000

    ui-updates

--- components/chat-area.tsx
+++ components/chat-area.tsx
@@ -5,7 +5,7 @@ import { Chat } from "@prisma/client";
 export const ChatArea = ({ chatList }: { chatList: Chat[] }) => {
   return (
     <div className="mx-auto max-w-3xl w-full h-full flex-1 flex flex-col">
-      <div className="flex-1 space-y-10 p-4 max-h-[calc(100svh-280px)] overflow-auto">
+      <div className="flex-1 space-y-10 p-4 max-h-[calc(100svh-146px)] pt-[146px] overflow-auto">
         {chatList.map((i, idx) => (
           <ChatItem key={idx} {...i} />
         ))}
