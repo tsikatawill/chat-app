d239936dda1471899de5d17c49918f48e017d1f0
Zerebel <86872654+Zerebel@users.noreply.github.com>
2025-03-09 16:25:31 +0000

    Develop (#3)
    * chore: updates
    
    * feat: implement server actions for chat functionality
    
    * chore: update Prisma migration for Chat schema
    
    * updates
    
    * chore: configure Open Next and Cloudflare deployment
    
    * chore: update .gitignore with open-next build directory (#1)
    
    * feat: ui updates
    
    * chore: update .gitignore with open-next build directory
    
    * Merge branch 'develop' into ui-revamp
    
    * feat: improve chat form and message display
    
    * feat: update chat area
    
    * refactor: extract chat header and list components
    
    ---------
    
    Co-authored-by: William M. Tsikata <tsikatawill@gmail.com>

---
+++ components/chat-list.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useRef, useLayoutEffect } from "react";
+import { ChatItem } from "./chat-item";
+import { Chat } from "@prisma/client";
+
+function ChatList({ chatList }: { chatList: Chat[] }) {
+    const chatListRef = useRef<HTMLDivElement>(null);
+
+    useLayoutEffect(() => {
+        if (chatListRef.current) {
+            chatListRef.current.scrollTop = chatListRef.current.scrollHeight;
+        }
+    }, [chatList]);
+
+    return (
+        <div
+            className="flex-1 space-y-10 p-4 max-h-[calc(100svh-280px)] overflow-auto"
+            ref={chatListRef}
+        >
+            {chatList.map((i, idx) => (
+                <ChatItem key={idx} {...i} />
+            ))}
+        </div>
+    );
+}
+
+export default ChatList;
