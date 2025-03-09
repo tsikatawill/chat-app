dc9df77940532eac11c8e8425b54c622f912d12d
Zerebel <86872654+Zerebel@users.noreply.github.com>
2025-03-09 15:51:09 +0000

    feat: improve chat form and message display

--- components/chat-form.tsx
+++ components/chat-form.tsx
@@ -1,21 +1,25 @@
 "use client";
-import { Send } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { sendMessage } from "@/actions/chat-actions";
 import Image from "next/image";
+import { useRef } from "react";
 
 export const ChatForm = () => {
+    const formRef = useRef<HTMLFormElement>(null);
+
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
         const formData = new FormData(e.target as HTMLFormElement);
         const message = formData.get("message");
         sendMessage(message as string);
+        formRef.current?.reset();
     }
 
     return (
         <form
             onSubmit={handleSubmit}
+            ref={formRef}
             className="flex p-3 items-center gap-4 w-full bg-whit bg-white rounded-full"
         >
             <Input
