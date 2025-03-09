c47f7f03d617654c2cb02c9d1d0eb7fd9c8c9500
William M. Tsikata <tsikatawill@gmail.com>
2025-03-09 18:29:59 +0000

    feat: update header
    add theme switch

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,19 @@ import ChatHeader from "@/components/chat-header";
 export default async function Home() {
   const data = await getMessages();
 
-    return (
-        <>
-            <ChatHeader />
-            <div
-                className="h-screen flex flex-col overflow-hidden"
-                style={{
-                    background:
-                        "url(images/main-bg.jpg) center center/cover no-repeat",
-                }}
-            >
-                <ChatArea chatList={data} />
-                <p className="text-center py-6">Built with ❤️ by Team Wazaaa</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <ChatHeader />
+      <div
+        className="h-screen flex flex-col overflow-hidden dark:bg-[url(/images/main-bg.jpg)_center_center/cover_no-repeat]"
+        // style={{
+        //     background:
+        //         " center center/cover no-repeat",
+        // }}
+      >
+        <ChatArea chatList={data} />
+        <p className="text-center py-6">Built with ❤️ by Team Wazaaa</p>
+      </div>
+    </>
+  );
 }
