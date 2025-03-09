31b587d7fdecfec9e3094b2bc778598fbd4105e7
William M. Tsikata <tsikatawill@gmail.com>
2025-03-09 14:34:17 +0000

    updates

--- components/chat-item.tsx
+++ components/chat-item.tsx
@@ -5,19 +5,20 @@ import dayjs from "dayjs";
 import "react-initials-avatar/lib/ReactInitialsAvatar.css";
 import { cn } from "@/lib/utils";
 import { type ChatItem as ChatItemProps } from "@/lib/types";
+import { Chat } from "@prisma/client/edge";
 
-export const ChatItem = ({ from, time, readStatus, text }: ChatItemProps) => {
-  const userName = "Alice John";
+export const ChatItem = ({ createdAt, message, id }: Chat) => {
+  const userId = "EB1";
 
   return (
-    <div className={cn("flex gap-4", userName === from && "flex-row-reverse")}>
-      <InitialsAvatar name={from} />
+    <div className={cn("flex gap-4", id === userId && "flex-row-reverse")}>
+      <InitialsAvatar name={"John Doe"} />
       <div>
         <div className="p-2 bg-white border rounded-lg max-w-lg min-w-xs">
-          {text}
+          {message}
         </div>
         <div className="flex justify-between mt-2">
-          <small>{dayjs(time).format("DD MMM - HH:mma")}</small>
+          <small>{dayjs(createdAt).format("DD MMM - HH:mma")}</small>
 
           {readStatus === "pending" ? (
             <CheckCheck />
