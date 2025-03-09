3dabc7c1c7cccc1c076ae10d16efe9b10fbb201e
Zerebel <86872654+Zerebel@users.noreply.github.com>
2025-03-09 19:19:45 +0000

    feat: Add Zustand for managing pending chat messages

--- components/chat-form.tsx
+++ components/chat-form.tsx
@@ -2,48 +2,52 @@
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { sendMessage } from "@/actions/chat-actions";
+import useChatStore from "@/lib/store/chat-store";
 import Image from "next/image";
 import { useRef } from "react";
 import { toast } from "sonner";
 
 export const ChatForm = () => {
-  const formRef = useRef<HTMLFormElement>(null);
+    const formRef = useRef<HTMLFormElement>(null);
+    const { addPendingChat } = useChatStore();
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
 
-    const formData = new FormData(e.target as HTMLFormElement);
-    const message = formData.get("message");
+        const formData = new FormData(e.target as HTMLFormElement);
+        const message = formData.get("message");
 
-    if (message && message?.toString().trim() === "") {
-      toast.error("Enter a message to send");
-      return;
+        if (message && message?.toString().trim() === "") {
+            toast.error("Enter a message to send");
+            return;
+        }
+
+        addPendingChat(message as string);
+        sendMessage(message as string);
+        formRef.current?.reset();
     }
-    sendMessage(message as string);
-    formRef.current?.reset();
-  }
 
-  return (
-    <form
-      onSubmit={handleSubmit}
-      ref={formRef}
-      className="flex p-3 items-center gap-4 w-full dark:bg-[#1C1C1C] bg-white rounded-full"
-    >
-      <Input
-        type="text"
-        name="message"
-        placeholder="Type a message"
-        className="border-none dark:placeholder:text-white/30 focus-visible:ring-0 shadow-none py-0 h-auto"
-      />
-      <Button className="rounded-full shrink-0 size-8 p-2 dark:invert">
-        <Image
-          className="dark:invert"
-          src="images/send-02.svg"
-          width={20}
-          height={20}
-          alt="."
-        />
-      </Button>
-    </form>
-  );
+    return (
+        <form
+            onSubmit={handleSubmit}
+            ref={formRef}
+            className="flex p-3 items-center gap-4 w-full dark:bg-[#1C1C1C] bg-white rounded-full"
+        >
+            <Input
+                type="text"
+                name="message"
+                placeholder="Type a message"
+                className="border-none dark:placeholder:text-white/30 focus-visible:ring-0 shadow-none py-0 h-auto"
+            />
+            <Button className="rounded-full shrink-0 size-8 p-2 dark:invert">
+                <Image
+                    className="dark:invert"
+                    src="images/send-02.svg"
+                    width={20}
+                    height={20}
+                    alt="."
+                />
+            </Button>
+        </form>
+    );
 };
