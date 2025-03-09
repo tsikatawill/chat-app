62beef1fd31d5a9fc2882852c3c6a842bffe0b01
Zerebel <86872654+Zerebel@users.noreply.github.com>
2025-03-09 16:21:56 +0000

    refactor: extract chat header and list components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { ChatArea } from "@/components/chat-area";
 import { getMessages } from "@/actions/chat-actions";
 import Image from "next/image";
+import ChatHeader from "@/components/chat-header";
 
 export default async function Home() {
     const data = await getMessages();
@@ -13,28 +14,8 @@ export default async function Home() {
                     "url(images/main-bg.jpg) center center/cover no-repeat",
             }}
         >
-            <div className="py-[26px] bg-white/20 backdrop-blur-lg sticky top-0 w-full">
-                <Image
-                    src={"images/logo.svg"}
-                    alt="logo"
-                    height={41}
-                    width={56}
-                    className="mx-auto"
-                />
-                <h1 className="font-semibold text-center text-2xl py-2">
-                    BuildSync
-                </h1>
-                <div className="flex justify-center gap-4 text-sm text-gray-500">
-                    <p>123 Members</p>
-                    <div className="flex items-center gap-2">
-                        <span className="size-2 block rounded-full bg-green-500"></span>{" "}
-                        200 online
-                    </div>
-                </div>
-            </div>
-
+            <ChatHeader />
             <ChatArea chatList={data} />
-
             <p className="text-center py-6">Built with ❤️ by Team Wazaaa</p>
         </div>
     );
--- components/chat-area.tsx
+++ components/chat-area.tsx
@@ -1,17 +1,12 @@
-import { ChatItem } from "./chat-item";
 import { ChatForm } from "./chat-form";
 import { Chat } from "@prisma/client";
+import ChatList from "./chat-list";
 
 export const ChatArea = ({ chatList }: { chatList: Chat[] }) => {
-  return (
-    <div className="mx-auto max-w-3xl w-full h-full flex-1 flex flex-col">
-      <div className="flex-1 space-y-10 p-4 max-h-[calc(100svh-280px)] overflow-auto">
-        {chatList.map((i, idx) => (
-          <ChatItem key={idx} {...i} />
-        ))}
-      </div>
-
-      <ChatForm />
-    </div>
-  );
+    return (
+        <div className="mx-auto max-w-3xl w-full h-full flex-1 flex flex-col">
+            <ChatList chatList={chatList} />
+            <ChatForm />
+        </div>
+    );
 };
