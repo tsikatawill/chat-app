9afc85560e3693eeab8877195a82f8b2ff387a74
Zerebel <86872654+Zerebel@users.noreply.github.com>
2025-03-09 20:43:50 +0000

    feat: Disable page revalidation for dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import { ChatArea } from "@/components/chat-area";
 import ChatHeader from "@/components/chat-header";
 
 export default async function Home() {
-  return (
-    <>
-      <ChatHeader />
-      <div className="h-screen flex flex-col overflow-hidden bg-[url('/images/main-bg.jpg')] dark:bg-[image:var(--background)] bg-center bg-cover bg-no-repeat">
-        <ChatArea />
-        <p className="text-center py-6">Built with ❤️ by Team Wazaaa</p>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <ChatHeader />
+            <div className="h-screen flex flex-col overflow-hidden bg-[url('/images/main-bg.jpg')] dark:bg-[image:var(--background)] bg-center bg-cover bg-no-repeat">
+                <ChatArea />
+                <p className="text-center py-6">Built with ❤️ by Team Wazaaa</p>
+            </div>
+        </>
+    );
 }
+
+export const revalidate = 0;
