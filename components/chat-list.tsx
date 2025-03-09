dbf415b153af5a5c9d62e99aba2089197526c4ed
William M. Tsikata <tsikatawill@gmail.com>
2025-03-09 16:42:25 +0000

    ui revamp

--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -4,24 +4,24 @@ import { ChatItem } from "./chat-item";
 import { Chat } from "@prisma/client";
 
 function ChatList({ chatList }: { chatList: Chat[] }) {
-    const chatListRef = useRef<HTMLDivElement>(null);
+  const chatListRef = useRef<HTMLDivElement>(null);
 
-    useLayoutEffect(() => {
-        if (chatListRef.current) {
-            chatListRef.current.scrollTop = chatListRef.current.scrollHeight;
-        }
-    }, [chatList]);
+  useLayoutEffect(() => {
+    if (chatListRef.current) {
+      chatListRef.current.scrollTop = chatListRef.current.scrollHeight;
+    }
+  }, [chatList]);
 
-    return (
-        <div
-            className="flex-1 space-y-10 p-4 max-h-[calc(100svh-280px)] overflow-auto"
-            ref={chatListRef}
-        >
-            {chatList.map((i, idx) => (
-                <ChatItem key={idx} {...i} />
-            ))}
-        </div>
-    );
+  return (
+    <div
+      className="flex-1 space-y-10 p-4 max-h-[calc(100svh-120px)] overflow-auto pt-[146px]"
+      ref={chatListRef}
+    >
+      {chatList.map((i, idx) => (
+        <ChatItem key={idx} {...i} />
+      ))}
+    </div>
+  );
 }
 
 export default ChatList;
